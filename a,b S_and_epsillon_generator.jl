681c41e5ebf6fa870a8078081216dc5dd173520b
Samavi Farnush Bint E Naser <47671556+SamaviFarnush@users.noreply.github.com>
2019-03-12 16:59:59 -0400

    Update a,b S_and_epsillon_generator.jl

--- a,b S_and_epsillon_generator.jl
+++ a,b S_and_epsillon_generator.jl
@@ -1,11 +1,12 @@
 include("data_urea_maximization.jl")
 include("Flux.jl")
 using DelimitedFiles
+
 #check_reaction
 S = stoichiometric_matrix[:,1:6];      ## w/o transfer reaction
 epsilon = atom_matrix*S;
 ##eplison_sum = sum(epsilon,dims=(2));
-(number_of_species, index) = size(S);
+
 writedlm("epsilon.txt",epsilon,"    ");
 writedlm("stoichiometric_matrix.csv",stoichiometric_matrix,",");
 writedlm("atom_matrix.csv",atom_matrix,",");
